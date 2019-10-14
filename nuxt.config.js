a897b56bab7863226d7a8118bebbd9e6f9db3825
Nora Panagiotidou <leonora@unumotors.com>
2019-10-14 14:52:58 +0300

    Add gsv

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {
+        hid: 'gsv',
+        name:'google-site-verification',
+        content: 'xsnOPtzeoSjSU1breuFRO3KsMn3AXHUuhfHwQYmV4cI'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
