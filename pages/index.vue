56dea716a5bb84e6c0dc9c8ea28697382e337810
Nora Panagiotidou <leonora@unumotors.com>
2019-10-18 23:16:15 +0300

    Setup work

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,17 @@
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
-<template>
-<div>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Σύντομα κοντά σας
-      </h1>
-      </div>
-  </div>
-        <div class="attribution">Icons made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
-  </div>
+<template lang="pug">
+main
+  section
+    .container
+      address
+        h1 Ελίζα Ι. Παναγιωτίδου
+        h2 Χειρουργός Οδοντίατρος
 </template>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  padding: 0 20px;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 3rem;
-  color: #35495e;
-  margin: 30px 0;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-
-.attribution {
-  font-size: 12px;
-  text-align: center;
-}
 </style>
